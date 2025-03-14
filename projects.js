a9351e492e2504196b096aa9e75ebd0856c08222
Harshith0509 <kasthuriharshith02@gmail.com>
2025-03-14 11:42:15 -0400

    Initial commit - Cleaned history

---
+++ projects.js
@@ -0,0 +1,40 @@
+const projects = [
+  
+    {
+      title: "Movie E-Booking System",
+      category: "web development",
+      link: "https://github.com/Harshith0509/Movie-Booking-system-using-PERN/",
+      imgSrc: "https://github.com/Harshith0509/Images/blob/main/moviebooking.webp?raw=true",
+      imgAlt: "Booking System"
+    },
+    {
+      title: "FlashCard Study Tool",
+      category: "web development",
+      link: "https://github.com/Harshith0509/Flashcard-Study-Tool/",
+      imgSrc: "https://github.com/Harshith0509/Images/blob/main/Flashcards.png?raw=true",
+      imgAlt: "FlashCard Study Tool"
+    },
+    {
+      title: "Hand Gesture Recognition to control MediaPlayers",
+      category: "Computer Vision",
+      link: "https://github.com/Harshith0509/Hand-Gesture-Recogniton-to-control-media-players/",
+      imgSrc: "https://github.com/Harshith0509/Images/blob/main/hand%20gesture.png?raw=true",
+      imgAlt: "Hand Gestures"
+    },    
+    {
+      title: "Multilingual QA Bot for College Website using BERT",
+      category: "Natural Language Processing",
+      link: "https://github.com/Harshith0509/Multilingual-Q-A-Bot-for-College-Website-using-BERT/",
+      imgSrc: "https://github.com/Harshith0509/Images/blob/main/AI%20bot.png?raw=true",
+      imgAlt: "QA Bot"
+    },
+    {
+      title: "House-Prices-Prediction",
+      category: "Data Analytics",
+      link: "https://github.com/Harshith0509/House-Prices-Prediction/",
+      imgSrc: "https://github.com/Harshith0509/Images/blob/main/house prices.jpg?raw=true",
+      imgAlt: "House Prices Prediction"
+    },   
+  ];
+
+  export default projects;
